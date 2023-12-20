2387fb08d879a05b97ec87351060b69a6963aeeb
luyongshou <louis@easyea.com>
2023-12-20 17:22:39 +0800

    修改toolchains的类型

--- jdk12/jdk12u-jdk-12.0.2+10/build.sh
+++ jdk12/jdk12u-jdk-12.0.2+10/build.sh
@@ -2,8 +2,8 @@
 env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin bash ./configure \
 --enable-unlimited-crypto --with-boot-jdk=/usr/jdk/instances/jdk11 \
 --with-native-debug-symbols=none \
---with-toolchain-type=gcc \
+--with-toolchain-type=solstudio \
 --disable-hotspot-gtest --disable-dtrace \
 --disable-warnings-as-errors
 
-env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake all
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin:/data/download/developerstudio12.6/bin gmake all
